39bb628be6496d66915eaada0c3447ffc6aaa71b
jonik2909 <boronovjonibek@gmail.com>
2022-09-10 05:01:39 +0900

    feat: create and read operations && build frontend javascript

--- app.js
+++ app.js
@@ -9,15 +9,33 @@ app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-// 2: Session code
+// 2: Session code 
 // 3: Views code
 app.set('views', 'views');
 app.set('view engine', 'ejs');
 
 // 4: Router code
 app.get("/", (req, res) => {
-    res.send("SERVER STARTED")
+   db.collection('plans').find().toArray((err, data) => {
+    if (err) {
+        console.log(err);
+        res.end('something went wrong!')
+    } else {
+        res.render('reja', {items: data})
+    }
+   })
 });
+
+app.post('/create-item', (req, res) => {
+    const new_reja = req.body.reja;
+    db.collection('plans').insertOne({reja: new_reja}, (err, data) => {
+        res.json(data.ops[0]);
+    })
+})
+
+
+
+ 
 
 
 module.exports = app;
